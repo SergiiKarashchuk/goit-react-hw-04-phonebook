d54c355ecc82b9042e6e62c78698c407a4fdef3c
Сергей Каращук <kspost01@gmail.com>
2023-03-07 19:52:29 +0200

    refactoring to huk

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
-import React, { Component } from 'react';
+import { useState } from 'react';
+import { useReactLocalStorage } from '../reactjs-localstorage';
 import { nanoid } from 'nanoid';
 import ContactForm from 'components/ContactForm';
 import ContactList from 'components/ContactList';
@@ -12,93 +13,83 @@ import {
   SectionTitle,
 } from './App.styled';
 
-class App extends Component {
-  state = {
-    contacts: [],
-    filter: '',
-  };
+function App() {
+  const [contacts, setContacts] = useReactLocalStorage('contacts', []);
+  const [filter, setFilter] = useState('');
 
-  componentDidMount() {
-    const savedContacts = JSON.parse(localStorage.getItem('contacts'));
+  // componentDidMount() {
+  //   const savedContacts = JSON.parse(localStorage.getItem('contacts'));
 
-    if (savedContacts) {
-      this.setState({ contacts: savedContacts });
-    }
-  }
+  //   if (savedContacts) {
+  //     this.setState({ contacts: savedContacts });
+  //   }
+  // }
 
-  componentDidUpdate(prevProps, prevState) {
-    const currentContacts = this.state.contacts;
+  // componentDidUpdate(prevProps, prevState) {
+  //   const currentContacts = this.state.contacts;
 
-    if (currentContacts !== prevState.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(currentContacts));
-    }
-  }
+  //   if (currentContacts !== prevState.contacts) {
+  //     localStorage.setItem('contacts', JSON.stringify(currentContacts));
+  //   }
+  // }
 
-  addContact = ({ name, number }) => {
+  const addContact = ({ name, number }) => {
     const contact = { id: nanoid(4), name, number };
     const normalizedName = name.toLowerCase();
 
     if (
-      this.state.contacts.find(
-        contact => contact.name.toLowerCase() === normalizedName
-      )
+      contacts.find(contact => contact.name.toLowerCase() === normalizedName)
     ) {
       return alert(`${name} is already in contacts!`);
     }
 
-    this.setState(prevState => ({
-      contacts: [contact, ...prevState.contacts],
-    }));
+    setContacts(prevContacts => [contact, ...prevContacts]);
   };
 
-  deleteContact = contactId => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
-    }));
+  const deleteContact = contactId => {
+    setContacts(prevContacts =>
+      prevContacts.filter(contact => contact.id !== contactId)
+    );
   };
 
-  filterContacts = event => {
-    this.setState({ filter: event.currentTarget.value });
+  const filterContacts = event => {
+    setFilter(event.currentTarget.value);
   };
 
-  getFilteredContacts = () => {
-    const { filter, contacts } = this.state;
+  const getFilteredContacts = () => {
     const normalizedFilter = filter.toLowerCase();
     return contacts.filter(contact =>
       contact.name.toLowerCase().includes(normalizedFilter)
     );
   };
 
-  render() {
-    const filteredContacts = this.getFilteredContacts();
-    const { addContact, filterContacts, deleteContact, state } = this;
+  const filteredContacts = getFilteredContacts();
 
-    return (
-      <Container>
-        <Title>Phonebook</Title>
-        <SectionsContainer>
-          <Section>
-            <SectionTitle>Add contact</SectionTitle>
-            <ContactForm onSubmit={addContact} />
-          </Section>
-          <Section className="contacts">
-            <SectionTitle>Contacts</SectionTitle>
-            {state.contacts.length !== 0 ? (
-              <>
-                <Filter value={state.filter} onChange={filterContacts} />
-                <ContactList
-                  contacts={filteredContacts}
-                  onDeleteButton={deleteContact}
-                />
-              </>
-            ) : (
-              <Message message="There are no contacts in your phonebook. Please add your first contact!" />
-            )}
-          </Section>
-        </SectionsContainer>
-      </Container>
-    );
-  }
+  return (
+    <Container>
+      <Title>Phonebook</Title>
+      <SectionsContainer>
+        <Section>
+          <SectionTitle>Add contact</SectionTitle>
+          <ContactForm onSubmit={addContact} />
+        </Section>
+        <Section className="contacts">
+          <SectionTitle>Contacts</SectionTitle>
+          {contacts.length !== 0 ? (
+            <>
+              <Filter value={filter} onChange={filterContacts} />
+              <ContactList
+                contacts={filteredContacts}
+                onDeleteButton={deleteContact}
+              />
+            </>
+          ) : (
+            <Message message="There are no contacts in your phonebook. Please add your first contact!" />
+          )}
+        </Section>
+      </SectionsContainer>
+    </Container>
+  );
 }
 
 export default App;
